7b37091fc4c0a2b705c9e3ef3126420f9d63997f
samad <abdessamad.bouchelga@gmail.com>
2023-03-17 10:10:08 -0700

    different combinations of three digits

--- 0x01-variables_if_else_while/101-print_comb4.c
+++ 0x01-variables_if_else_while/101-print_comb4.c
@@ -1,39 +1,35 @@
 #include <stdio.h>
-
 /**
  * main - Entry point
  *
- * Description: Prints all possible different combinations of three digits
- * separated by ',', followed by a space. The three digits must be different.
- * 012, 120, 102, 021, 201, 210 are considered the same combination of the
- * three digits 0, 1 and 2. Prints only the smallest combination of three digits.
- * Numbers should be printed in ascending order, with three digits.
- * Only putchar is allowed, and can be used at most six times.
+ * Description: This function will print the all combination of
+ * Three digits without repeating the combination by using putchar
  *
- * Return: Always 0 (Success)
+ * Return: 0 (Success)
  */
 int main(void)
 {
-    int i, j, k;
+	int first;
+	int second;
+	int third;
 
-    for (i = 0; i <= 7; i++)
-    {
-        for (j = i + 1; j <= 8; j++)
-        {
-            for (k = j + 1; k <= 9; k++)
-            {
-                putchar(i % 8 + '0');
-                putchar(j % 9 + '0');
-                putchar(k % 10 + '0');
-                if (i < 7)
-                {
-                    putchar(',');
-                    putchar(' ');
-                }
-            }
-        }
-    }
-    putchar('\n');
-
-    return (0);
+	for (first = 0; first < 8; first++)
+	{
+		for (second = first + 1; second < 9; second++)
+		{
+			for (third = second + 1; third < 10; third++)
+			{
+				putchar(first + '0');
+				putchar(second + '0');
+				putchar(third + '0');
+				if (first < 7 || second < 8 || third < 9)
+				{
+					putchar(',');
+					putchar(' ');
+				}
+			}
+		}
+	}
+	putchar('\n');
+	return (0);
 }
