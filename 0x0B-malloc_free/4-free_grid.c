0529c4f459fefbe5b7a053cb1c0572249ca52102
samad <abdessamad.bouchelga@gmail.com>
2023-04-11 13:34:11 -0700

    2d grid

--- 0x0B-malloc_free/4-free_grid.c
+++ 0x0B-malloc_free/4-free_grid.c
@@ -10,7 +10,7 @@ void free_grid(int **grid, int height)
 {
 	int a;
 
-	if (grid == NULL || heghit <= 0)
+	if (grid == NULL || height <= 0)
 		return;
 	for (a = 0; a < height; a++)
 		free(grid[a]);
