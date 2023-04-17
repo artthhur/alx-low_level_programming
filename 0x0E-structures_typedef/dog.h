e841fc8fbc085ba32b32ad59893a6d59cd1b0424
samad <abdessamad.bouchelga@gmail.com>
2023-04-17 14:32:57 -0700

    dog.h

--- 0x0E-structures_typedef/dog.h
+++ 0x0E-structures_typedef/dog.h
@@ -1,10 +1,10 @@
-#ifndef DOG_F
+#ifndef DOG_H
 #define DOG_H
 /**
  * struct dog - a struct representing a dog
  * @name: the name of the dog
  * @age: the age of the dog
- * @owner: the name of the dog's owner
+ * @owner: the owner ot dog
  */
 
 struct dog
