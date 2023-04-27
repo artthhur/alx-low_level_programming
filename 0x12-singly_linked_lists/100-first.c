65ad40a7d7ea1766996b30197a5c58ba5f8886b6
samad <abdessamad.bouchelga@gmail.com>
2023-04-27 15:11:36 -0700

    solahfat

---
+++ 0x12-singly_linked_lists/100-first.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+void solahfat(void)__attribute__((constructor));
+/**
+ * solahfat - prints You're beat! and yet, you must allow,
+ * I bore my house upon my back!
+ */
+void solahfat(void)
+{
+	printf("You're beat! and yet, you must allow,\n");
+	printf("I bore my house upon my back!\n");
+}
