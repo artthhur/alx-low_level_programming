bf65309555e70c12159c76e628d22b6f15860f42
samad <abdessamad.bouchelga@gmail.com>
2023-05-02 15:12:52 -0700

    function that frees a listint_t list

--- 0x13-more_singly_linked_lists/102-free_listint_safe.c
+++ 0x13-more_singly_linked_lists/102-free_listint_safe.c
@@ -19,7 +19,7 @@ const listint_t **m(const listint_t **l, size_t size, const listint_t *newnd)
 		exit(98);
 	}
 	for (i = 0; i < size - 1; i++)
-		newl[i] = l[i];
+		new_list[i] = l[i];
 	new_list[i] = newnd;
 	free(l);
 	return (new_list);
