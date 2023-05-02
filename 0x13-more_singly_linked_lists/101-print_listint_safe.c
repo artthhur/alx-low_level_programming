0b0b20b67d35b190572d39acb9599962565f6e46
samad <abdessamad.bouchelga@gmail.com>
2023-05-02 14:55:49 -0700

    function that prints a listint_t linked list

---
+++ 0x13-more_singly_linked_lists/101-print_listint_safe.c
@@ -0,0 +1,56 @@
+#include "lists.h"
+/**
+ * mem - allocate memorry for an array
+ * of pointers to nodes in linked list
+ * @l: old list
+ * @size: size of new list
+ * @newnd: new node to add
+ * Return: pointer to the new list
+ */
+const listint_t **mem(const listint_t **l, size_t size, const listint_t *newnd)
+{
+	const listint_t **new_list;
+	size_t i;
+
+	new_list = malloc(size * sizeof(listint_t *));
+	if (new_list == NULL)
+	{
+		free(l);
+		exit(98);
+	}
+	for (i = 0; i < size - 1; i++)
+		newl[i] = l[i];
+	new_list[i] = new_node;
+	free(l);
+	return (new_list);
+}
+/**
+ * print_listint_safe - print list
+ * @head: head
+ * Return: number of node in the list
+ */
+
+size_t print_listint_safe(const listint_t *head)
+{
+	size_t i, number_list = 0;
+	const listint_t **List = NULL;
+
+	while (head != NULL)
+	{
+		for (i = 0; i < number_list; i++)
+		{
+			if (head == List[i])
+			{
+				printf("-> [%p] %d\n", (void *)head, head->n);
+				free(List);
+				return (number_list);
+			}
+		}
+		number_list++;
+		List = mem(List, number_list, head);
+		printf("[%p] %d\n", (void *)head, head->number_list);
+		head = head->next;
+	}
+	free(List);
+	return (number_list);
+}
