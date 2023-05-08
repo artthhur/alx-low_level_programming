147530adc638d758ed1b4d341064d3260eb9dd10
samad <abdessamad.bouchelga@gmail.com>
2023-05-08 16:30:51 -0700

    print and read

--- 0x15-file_io/0-read_textfile.c
+++ 0x15-file_io/0-read_textfile.c
@@ -23,7 +23,7 @@ ssize_t read_textfile(const char *filename, size_t letters)
 		return (0);
 	}
 	lenr = read(filep, buffer, letters);
-	close(ptfile);
+	close(filep);
 	if (lenr == -1)
 	{
 		free(buffer);
