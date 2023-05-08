9c6f9af2114f5fffbdf76b13b56037784dc6fcce
samad <abdessamad.bouchelga@gmail.com>
2023-05-08 16:46:57 -0700

    append

---
+++ 0x15-file_io/2-append_text_to_file.c
@@ -0,0 +1,35 @@
+#include "main.h"
+/**
+ * _strlen - length of stringg
+ * @s: string
+ * Return: length of string
+ */
+size_t _strlen(char *s)
+{
+	size_t i;
+
+	for (i = 0; s[i]; i++)
+		;
+	return (i);
+}
+/**
+ * append_text_to_file - append text to file
+ * @filename: file name
+ * @text_content: text to append
+ * Return: 1 success and -1 failure
+ */
+int append_text_to_file(const char *filename, char *text_content)
+{
+	int filep;
+	ssize_t ln;
+
+	filep = open(filename, O_WRONLY | O_APPEND);
+	if filep == -1)
+		return (-1);
+	if (text_content != NULL)
+		ln = write(filep, text_content, _strlen(text_content));
+	close(filrp);
+	if (ln == -1)
+		return (-1);
+	return (1);
+}
