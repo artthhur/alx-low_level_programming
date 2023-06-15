7629e610afe99f5d1cbd4cc3ee2940b08951b032
samad <abdessamad.bouchelga@gmail.com>
2023-06-15 14:00:28 -0700

    print dlistint

--- 0x17-doubly_linked_lists/0-print_dlistint.c
+++ 0x17-doubly_linked_lists/0-print_dlistint.c
@@ -8,15 +8,15 @@
  */
 size_t print_dlistint(const dlistint_t *head)
 {
-    const dlistint_t *current_node = head;
-    size_t node_count = 0;
+	const dlistint_t *current_node = head;
+	size_t node_count = 0;
 
-    while (current_node != NULL)
-    {
-        printf("%d\n", current_node->n);
-        current_node = current_node->next;
-        node_count++;
-    }
+	while (current_node != NULL)
+	{
+		printf("%d\n", current_node->n);
+		current_node = current_node->next;
+		node_count++;
+	}
 
-    return (node_count);
+	return (node_count);
 }
