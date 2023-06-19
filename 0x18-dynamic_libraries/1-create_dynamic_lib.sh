b093d91e91bf5acb922c3de092f7b7421de4ff7a
samad <abdessamad.bouchelga@gmail.com>
2023-06-19 16:32:16 -0700

    task 1

--- 0x18-dynamic_libraries/1-create_dynamic_lib.sh
+++ 0x18-dynamic_libraries/1-create_dynamic_lib.sh
@@ -1,10 +1,4 @@
 #!/bin/bash
-
-# Compile all .c files in the current directory into object files
-gcc -c -Wall -Werror -Wextra -pedantic -std=gnu89 *.c
-
-# Create the dynamic library liball.so
+gcc -Wall -Wextra -Werror -pedantic -c -fPIC *.c
 gcc -shared -o liball.so *.o
-
-# export library
 export LD_LIBRARY_PATH=.:$LD_LIBRARY_PATH
